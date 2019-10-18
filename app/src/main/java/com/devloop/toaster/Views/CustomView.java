58e2590ece0236628de8d0574ddb528c6dae4e9a
Joma Simeon <joma@Jomas-MacBook-Pro.local>
2019-10-18 11:20:40 +0300

    fine tunining

--- app/src/main/java/com/devloop/toaster/Views/CustomView.java
+++ app/src/main/java/com/devloop/toaster/Views/CustomView.java
@@ -4,27 +4,35 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
+import android.graphics.RectF;
 import android.view.View;
 
 public class CustomView extends View {
     Paint paint;
-    Rect rect;
+    RectF rect;
 
     public CustomView(Context context) {
         super(context);
-
+        /*
+            Paint is used to define styles and colors for a given
+            drawing.
+         */
         paint = new Paint();
         paint.setColor(Color.RED);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(3);
-        rect = new Rect();
+        rect = new RectF();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        /*
+            Canvas - 2D drawing API/framework offered by Android.
+            Canvas offers drawing methods and makes it possible to draw
+            items on a give canvas.
+         */
         super.onDraw(canvas);
-        rect.set(20, 20, getWidth(), getHeight());
+        rect.set(20, 20, getWidth() - 20, (float) (getHeight() * 0.05));
         canvas.drawRect(rect, paint);
     }
 }
